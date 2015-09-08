f04fc9d06a6ca9ac44807e4aa662aa41a35ed2e3
Hugh Brown <hughdbrown@yahoo.com>
2015-09-08 10:24:04 -0600

    Filter by hit count

--- script/04-get-webpages.py
+++ script/04-get-webpages.py
@@ -7,15 +7,44 @@ from __future__ import print_function, absolute_import, division
 
 from sys import stderr
 import concurrent.futures
+from csv import DictReader
 
 import simplejson
 from pymongo import MongoClient
 import requests
 
 
+# Number of hits across 72 hours by rank
+# 25k rank = 937 hits
+# 50k rank = 431 hits
+# 100k rank = 178 hits
+# 200k rank = 68 hits
+
+CUTOFF = 500
+
 client = MongoClient()
 db = client.data
 collection = db.urls
+
+FIELDNAMES = ('short_url', 'count')
+
+def url_counts():
+    """
+    Extract the number of times a particular URL was hit in 72 hours
+    from precomputed data. Use this to filter out low hit counts.
+    """
+    print("Opening CSV file of url counts", file=stderr)
+    with open("hits-csv/aggregated.csv") as f:
+        print("Reading CSV", file=stderr)
+        reader = DictReader(f, fieldnames=FIELDNAMES)
+        return {
+            row["short_url"]: int(row['count'])
+            for row in reader      
+        }
+
+
+# Read mapping of short url to number of hits into global variable
+URL_COUNTS = url_counts()
 
 
 def is_website_collateral(long_url):
@@ -24,11 +53,14 @@ def is_website_collateral(long_url):
 
 def load_url(args):
     short_url, long_url = args
-    doc = {'short_url': short_url, 'long_url': long_url}
+    count = URL_COUNTS[short_url]
+    doc = {'short_url': short_url, 'long_url': long_url, 'count': count}
     try:
         if is_website_collateral(long_url):
             # Skip urls that cannot have 
             doc['exc'] = 'Website collateral'
+        elif count < CUTOFF:
+            doc['exc'] = "Too few hits"
         else:
             # Record text scraped
             r = requests.get(long_url, timeout=12)
