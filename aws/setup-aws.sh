b8d5b1939be55b7756906ba6086a586f3c497203
Hugh Brown <hughdbrown@yahoo.com>
2015-09-11 12:14:00 -0600

    Fix up AWS script

--- aws/setup-aws.sh
+++ aws/setup-aws.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Run this on AWS
+
 cd ~
 mkdir -p scripts
 mkdir -d data
@@ -15,12 +17,12 @@ wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
 sudo pip install -r requirements.txt
 
-REGION="us-west-2"
-
-cd ~/data
-aws s3 cp --recursive --region $REGION s3://hughdbrown/data-capstone/data .
-aws s3 cp --recursive --region $REGION s3://hughdbrown/data-capstone/urls.bson .
-
-cd ~/script
-python 04-create-database.py
-mongorestore --db=data --collection=urls ../data/urls.bson
+#REGION="us-west-2"
+#
+#cd ~/data
+#aws s3 cp --recursive --region $REGION s3://hughdbrown/data-capstone/data .
+#aws s3 cp --recursive --region $REGION s3://hughdbrown/data-capstone/urls.bson .
+#
+#cd ~/script
+#python 04-create-database.py
+#mongorestore --db=data --collection=urls ../data/urls.bson
