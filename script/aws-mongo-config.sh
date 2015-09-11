4d29b4e026a6628a2c385491a9fe624952480606
Hugh Brown <hughdbrown@yahoo.com>
2015-09-10 21:38:14 -0600

    Amend AWS installation script for mongodb in redhat

--- script/aws-mongo-config.sh
+++ script/aws-mongo-config.sh
@@ -16,25 +16,43 @@ enabled=1" | sudo tee -a /etc/yum.repos.d/mongodb.repo
 sudo yum install -y mongodb-org-server mongodb-org-shell mongodb-org-tools
 
 # Make directoires
-sudo mkdir /data /log /journal
+sudo mkdir /data
+sudo mkdir /capstone
+
+DEVICE="/dev/xvdb"
 
 # Format space
-sudo mkfs.ext4 /dev/xvdf
+sudo mkfs.ext4 $DEVICE
 
 # Set /etc/fstab to configure the mountpoint(s)
-echo '/dev/xvdf /data ext4 defaults,auto,noatime,noexec 0 0' | sudo tee -a /etc/fstab
+echo "$DEVICE /data ext4 defaults,auto,noatime,noexec 0 0" | sudo tee -a /etc/fstab
+echo "/dev/xvdf /capstone ext4 defaults,auto,noatime,noexec 0 0" | sudo tee -a /etc/fstab
 
 # Mount drives
 sudo mount /data
+sudo mount /capstone
 
-sudo mkdir -p /data/{data,log,journal}
+sudo mkdir -p /data/{db,log,journal}
 
 # Chown the directories
-sudo chown mongod:mongod /data
+sudo chown -R mongod:mongod /data
 
 # Modify .etc/mongod.conf with directories
-echo 'dbpath = /data/data
-logpath = /data/log/mongod.log' | sudo tee -a /etc/mongod.conf
+sudo rm /etc/mongod.conf
+echo '
+systemLog:
+   destination: file
+   path: "/data/log/mongod.log"
+   logAppend: true
+storage:
+   dbPath: "/data/db"
+   journal:
+      enabled: true
+processManagement:
+   fork: true
+net:
+   bindIp: 127.0.0.1
+   port: 27017' | sudo tee -a /etc/mongod.conf
 
 # Specialized ulimit optimizations
 echo '* soft nofile 64000
@@ -46,9 +64,11 @@ echo '* soft nproc 32000
 * hard nproc 32000' | sudo tee -a /etc/security/limits.d/90-nproc.conf
 
 # Set the readahead appropriately
-sudo blockdev --setra 32 /dev/xvdf
+sudo blockdev --setra 32 $DEVICE
 
-echo 'ACTION=="add", KERNEL=="xvdf", ATTR{bdi/read_ahead_kb}="16"' | sudo tee -a /etc/udev/rules.d/85-ebs.rules
+echo 'ACTION=="add", KERNEL=="/dev/xvdb", ATTR{bdi/read_ahead_kb}="16"' | sudo tee -a /etc/udev/rules.d/85-ebs.rules
 
 # Start mongodb
 sudo service mongod start
+
+mongorestore /capstone/dump
