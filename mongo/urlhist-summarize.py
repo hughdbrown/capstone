8dacfccb0a828e5c68b21e1ecfba665b3ad15f33
Hugh Brown <hughdbrown@yahoo.com>
2015-09-14 18:30:57 -0600

    Format code better

--- mongo/urlhist-summarize.py
+++ mongo/urlhist-summarize.py
@@ -10,7 +10,10 @@ items = list(db.urlhist.find({}, {'timestamp': 1, 'country': 1, '_id': 0}))
 # >>> sum(1 for d in items if "country" not in d)
 # 10751
 
-c = Counter((d["timestamp"].day, d["timestamp"].hour, d["timestamp"].minute, d["country"]) for d in items if "country" in d)
+c = Counter(
+    (d["timestamp"].day, d["timestamp"].hour, d["timestamp"].minute, d["country"])
+    for d in items if "country" in d
+)
 t = [
     {'day': day, 'hour': hour, 'minute': minute, 'country': country, 'count': count}
     for (day, hour, minute, country), count in c.items()
@@ -18,7 +21,10 @@ t = [
 with open("urlhist-timestamp-country.json", "w") as f:
     f.write(simplejson.dumps(t))
 
-c = Counter((d["timestamp"].day, d["timestamp"].hour, d["timestamp"].minute) for d in items)
+c = Counter(
+    (d["timestamp"].day, d["timestamp"].hour, d["timestamp"].minute)
+    for d in items
+)
 t = [
     {'day': day, 'hour': hour, 'minute': minute, 'count': count}
     for (day, hour, minute), count in c.items()
