5d2532977ab82686b414f45afbe2d4456734e0a5
Hugh Brown <hughdbrown@yahoo.com>
2015-09-15 15:40:54 -0600

    Correct extraction shell scripts

--- script/02-extract-tz.sh
+++ script/02-extract-tz.sh
@@ -2,12 +2,12 @@
 
 # The second step in the project involved figuring out the most commonly hit URLs for each data file.
 
-DST_DIR="hits-csv"
+DST_DIR="timezones"
 [ ! -d "${DST_DIR}" ] && mkdir "${DST_DIR}"
 
 
 for file in $( ls ../data-capstone/* ); do
     csvfile=$(basename $file ".gz")."csv"
     echo $csvfile
-    zcat < "${file}" | python "extract-urls.py" --key=g > $DST_DIR/${csvfile}
+    zcat < "${file}" | python "extract-urls.py" --key=tz > $DST_DIR/${csvfile}
 done
